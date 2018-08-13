f1cdabc5bf2e923cbd6e8de076e107850083b170
Nycole <nycwalsh@gmail.com>
2018-08-13 15:16:35 -0500

    Near complete, missing trasncript click functionality

--- js/captions.js
+++ js/captions.js
@@ -1,15 +1,17 @@
-const vid = document.getElementsByTagName('video');
+const vid = document.getElementsByTagName('video')[0];
 const spans = document.getElementsByTagName('span');
 
 vid.ontimeupdate = () => {
-  for (let i = 0; i < spans.length; i += 1) {
+  for (let i = 0; i <= spans.length; i += 1) {
     const vidTime = vid.currentTime;
     if (vidTime > spans[i].getAttribute("data-start")
     && vidTime < spans[i].getAttribute("data-end")) {
-      spans[i].style.color = "#1B98C2";
+      spans[i].style.color = "#0E3F20";
+      spans[i].style.fontWeight = "bold";
     }
     else {
       spans[i].style.color = "#797e83";
+      spans[i].style.fontWeight = "normal";
     }
   }
 };
